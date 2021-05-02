383309105299ab5e21524839cc02588e30ef8982
Shafinaz Shafique <shafinaz2006@gmail.com>
2021-05-02 13:34:58 -0700

    deploy heroku- add path

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
-const express = require('express')
+const express = require('express');
 const app = express();
+const path = require('path');
 require('dotenv').config();
 const PORT = process.env.PORT || 8080;
 const password = process.env.GMAIL_PW;

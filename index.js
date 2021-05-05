ede4e4447d1a41edb7973c212dff87b142355988
Shafinaz Shafique <shafinaz2006@gmail.com>
2021-05-04 23:10:16 -0700

    fix to receive email from prod

--- index.js
+++ index.js
@@ -12,11 +12,10 @@ const cors = require('cors');
 app.use(cors());
 app.use(express.json());
 if(process.env.NODE_ENV === 'production') {
-    // set static folder
     app.use(express.static('client/build'));
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
+    // app.get('*', (req, res) => {
+    //   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    // });
 }
 
 const transporter = nodemailer.createTransport({
@@ -56,6 +55,4 @@ app.post('/contact', (req, res) =>{
 
 // server listening:
 
-app.listen(PORT, () => {
-    console.log(`server listening at ${PORT}`);
-})
+app.listen(PORT, () => console.log(`server listening at ${PORT}`));
